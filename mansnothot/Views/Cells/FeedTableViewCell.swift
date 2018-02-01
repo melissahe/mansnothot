0108900f177aa5f8fda806413aebe1af14569fa6
Richard Crichlow <richardcrichlow@ac.c4q.nyc>
2018-02-01 12:33:18 -0500

    Views are testable

--- mansnothot/Views/Cells/FeedTableViewCell.swift
+++ mansnothot/Views/Cells/FeedTableViewCell.swift
@@ -10,40 +10,56 @@ import UIKit
 import SnapKit
 import Kingfisher
 
-//TODO: Set up
+class FeedTableViewCell: UITableViewCell {
+    
     //add objects
-        //userImageView - for user image
-        //usernameLabel - for user name
-        //postImageView - image view in post
-        //textView - for post
-        //numberOfLikes label - number of likes
-        //thumbsUp button - adds to likes
-        //numberOfDislikes label - number of dislikes
-        //thumbsDown button - adds to dislikes
-        //comment button - should segue to AddCommentVC //actually i'm not sure if we're doing this anymore, refer to trello!
-        //showThread button - should display the total number of comments in button title - should segue to AllCommentsVC
-        //flag icon (which is a button) - that will present the flag action sheet
+    //userImageView - for user image
+    //usernameLabel - for user name
+    //postImageView - image view in post
+    //textView - for post
+    //numberOfLikes label - number of likes
+    //thumbsUp button - adds to likes
+    //numberOfDislikes label - number of dislikes
+    //thumbsDown button - adds to dislikes
+    //comment button - should segue to AddCommentVC //actually i'm not sure if we're doing this anymore, refer to trello!
+    //showThread button - should display the total number of comments in button title - should segue to AllCommentsVC
+    //flag icon (which is a button) - that will present the flag action sheet
     //set up constraints
 
-class FeedTableViewCell: UITableViewCell {
-
+    //label
+    lazy var usernameLabel: UILabel = {
+        let lb = UILabel()
+        lb.text = "Test"
+        lb.backgroundColor = .white
+        lb.textAlignment = .center
+        lb.textColor = .black
+        lb.numberOfLines = 0
+        lb.layer.borderWidth = 0.5
+        return lb
+    }()
+    
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-        commonInit()
+        super.init(style: style , reuseIdentifier: "FeedCell")
+        setupAndConstrainObjects()
     }
     
     required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
-    
-    private func commonInit() {
-        backgroundColor = .white
-        setUpViews()
-    }
-    
-    private func setUpViews() {
+        super.init(coder: aDecoder)
+        setupAndConstrainObjects()
         
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        self.backgroundColor = .clear
+        setupAndConstrainObjects()
+    }
+    
+    private func setupAndConstrainObjects(){
+        self.addSubview(usernameLabel)
+        usernameLabel.snp.makeConstraints { (make) -> Void in
+            make.edges.equalTo(self.snp.edges)
+        }
     }
     
 }
