defb969acc9d0a9785da41f1bfee1e28e29d50a8
Richard Crichlow <richardcrichlow@ac.c4q.nyc>
2018-02-02 11:17:31 -0500

    Looking good

--- mansnothot/Views/Cells/AllCommentsTableViewCell.swift
+++ mansnothot/Views/Cells/AllCommentsTableViewCell.swift
@@ -39,11 +39,43 @@ class AllCommentsTableViewCell: UITableViewCell {
         return tv
     }()
     
-    //nice to have:
-    //numberOfLikes label - number of likes
     //thumbsUp button - adds to likes
-    //numberOfDislikes label - number of dislikes
+    lazy var thumbsUpButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "thumbsUp"), for: .normal)
+        button.backgroundColor = .clear
+        button.layer.borderWidth = 0.5
+        return button
+    }()
+    
+    //numberOfLikes label - number of likes
+    lazy var numberOfLikesLabel: UILabel = {
+        let lb = UILabel()
+        lb.text = "+9"
+        lb.backgroundColor = .gray
+        lb.textAlignment = .center
+        lb.backgroundColor = .white
+        return lb
+    }()
+    
     //thumbsDown button - adds to dislikes
+    lazy var thumbsDownButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "thumbsDown"), for: .normal)
+        button.backgroundColor = .clear
+        button.layer.borderWidth = 0.5
+        return button
+    }()
+    
+    //numberOfDislikes label - number of dislikes
+    lazy var numberOfDislikesLabel: UILabel = {
+        let lb = UILabel()
+        lb.text = "-7"
+        lb.backgroundColor = .gray
+        lb.textAlignment = .center
+        lb.backgroundColor = .white
+        return lb
+    }()
     
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style , reuseIdentifier: "AllCommentsCell")
@@ -62,22 +94,58 @@ class AllCommentsTableViewCell: UITableViewCell {
     }
     
     private func setupAndConstrainObjects(){
+        self.addSubview(thumbsUpButton)
+        self.addSubview(numberOfLikesLabel)
+        self.addSubview(thumbsDownButton)
+        self.addSubview(numberOfDislikesLabel)
         self.addSubview(usernameLabel)
         self.addSubview(commentTextView)
         
+        thumbsUpButton.snp.makeConstraints { (make) -> Void in
+            make.top.equalTo(self.snp.top)
+            make.trailing.equalTo(self.snp.trailing)
+        }
+        
+        numberOfLikesLabel.snp.makeConstraints { (make) -> Void in
+            make.top.equalTo(thumbsUpButton.snp.bottom)
+            make.leading.equalTo(thumbsUpButton.snp.leading)
+            make.trailing.equalTo(thumbsUpButton.snp.trailing)
+            make.height.equalTo(thumbsUpButton.snp.height)
+        }
+        
+        thumbsDownButton.snp.makeConstraints { (make) -> Void in
+            make.top.equalTo(numberOfLikesLabel.snp.bottom)
+            make.leading.equalTo(thumbsUpButton.snp.leading)
+            make.trailing.equalTo(thumbsUpButton.snp.trailing)
+            make.height.equalTo(thumbsUpButton.snp.height)
+        }
+        
+        numberOfDislikesLabel.snp.makeConstraints { (make) -> Void in
+            make.top.equalTo(thumbsDownButton.snp.bottom)
+            make.leading.equalTo(thumbsUpButton.snp.leading)
+            make.trailing.equalTo(thumbsUpButton.snp.trailing)
+            make.height.equalTo(thumbsUpButton.snp.height)
+            make.bottom.equalTo(self.snp.bottom)
+        }
+        
+        //Update these contraints so that the trailing is set to the leading of the buttons
         usernameLabel.snp.makeConstraints { (make) -> Void in
             make.top.equalTo(self.snp.top)
             make.leading.equalTo(self.snp.leading)
-            make.trailing.equalTo(self.snp.trailing)
+            make.trailing.equalTo(thumbsUpButton.snp.leading)
         }
         
         commentTextView.snp.makeConstraints { (make) -> Void in
             make.top.equalTo(usernameLabel.snp.bottom)
-            make.centerX.equalTo(self.snp.centerX)
+            make.leading.equalTo(self.snp.leading)
+            make.trailing.equalTo(thumbsUpButton.snp.leading)
             make.bottom.equalTo(self.snp.bottom)
-            make.height.equalTo(self.snp.width).multipliedBy(0.1)
-            make.width.equalTo(self.snp.width)
+            
         }
+        
+        
+        
+        
     }
 
 }
