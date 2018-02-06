9c5d54fb0aa4b350d9df60743b189c392748b377
Melissa He <melissahe@ac.c4q.nyc>
2018-02-05 20:45:22 -0500

    Add keyboard/error handling, integrate Firebase with CreateAccountVC, and add custom Firebase delegates

--- mansnothot/View Controllers/CreateAccountVC.swift
+++ mansnothot/View Controllers/CreateAccountVC.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import SnapKit
+import FirebaseAuth
 
 //Purpose: for creating account with app
 
@@ -26,15 +27,26 @@ class CreateAccountVC: UIViewController {
         setupView()
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        self.view.endEditing(true)
+    }
+    
     private func setupView() {
         view.addSubview(createAccountView)
         createAccountView.emailTextField.delegate = self
         createAccountView.usernameTextField.delegate = self
         createAccountView.passwordTextField.delegate = self
         
+        //Action for Dismiss Button
+        createAccountView.dismissButton.addTarget(self, action: #selector(dismissButtonFunc), for: .touchUpInside)
+        
         //Action for Create Account Button
         createAccountView.createAccountButton.addTarget(self, action: #selector(newAccountFunc), for: .touchUpInside)
-        
+    }
+    
+    @objc private func dismissButtonFunc() {
+        self.navigationController?.dismiss(animated: true, completion: nil)
     }
     
     @objc private func newAccountFunc() {
@@ -44,40 +56,73 @@ class CreateAccountVC: UIViewController {
         
         //If user successfully creates a new account send them back to the Login Screen
         
-        let alert = UIAlertController(title: "Success!", message: "Account Created!", preferredStyle: .alert)
-        let ok = UIAlertAction(title: "Back to Login", style: .default, handler: {(UIAlertAction) -> Void in self.dismiss(animated: true, completion: nil)})
-        alert.addAction(ok)
-        present(alert, animated: true, completion: nil)
+        guard let usernameText = createAccountView.usernameTextField.text else {
+            createAccountView.statusLabel.text = "Please enter a valid Username"
+            return
+        }
+        guard let passwordText = createAccountView.passwordTextField.text else {
+            createAccountView.statusLabel.text = "Please enter a valid Password"
+            return
+        }
+        guard let emailText = createAccountView.emailTextField.text else {
+            createAccountView.statusLabel.text = "Please enter a valid Email"
+            return
+        }
         
+        AuthUserService.manager.delegate = self
+        AuthUserService.manager.createAccount(withEmail: emailText, password: passwordText, andDisplayName: usernameText) {
+            let alert = Alert.create(withTitle: "Error", andMessage: "This display name has already been used.", withPreferredStyle: .alert)
+            Alert.addAction(withTitle: "OK", style: .default, andHandler: nil, to: alert)
+            self.present(alert, animated: true, completion: nil)
+        }
     }
-    
     
 }
 extension CreateAccountVC: UITextFieldDelegate {
     func textFieldDidBeginEditing(_ textField: UITextField) {
-        
         becomeFirstResponder()
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        guard let usernameText = createAccountView.usernameTextField.text else {
-            createAccountView.statusLabel.text = "Please enter a valid Username"
-            return true
-        }
-        guard let passwordText = createAccountView.passwordTextField.text else {
-            createAccountView.statusLabel.text = "Please enter a valid Password"
-            return true
-        }
-        guard let emailText = createAccountView.emailTextField.text else {
-            createAccountView.statusLabel.text = "Please enter a valid Email"
-            return true
-        }
+//        guard let usernameText = createAccountView.usernameTextField.text else {
+//            createAccountView.statusLabel.text = "Please enter a valid Username"
+//            return true
+//        }
+//        guard let passwordText = createAccountView.passwordTextField.text else {
+//            createAccountView.statusLabel.text = "Please enter a valid Password"
+//            return true
+//        }
+//        guard let emailText = createAccountView.emailTextField.text else {
+//            createAccountView.statusLabel.text = "Please enter a valid Email"
+//            return true
+//        }
+//
+//        print(usernameText)
+//        print(passwordText)
+//        print(emailText)
         
-        print(usernameText)
-        print(passwordText)
-        print(emailText)
-        
-        resignFirstResponder()
+        textField.resignFirstResponder()
         return true
+    }
+    
+    func presentErrorAlert(withErrorMessage message: String) {
+        let alertController = Alert.createErrorAlert(withMessage: message)
+        self.present(alertController, animated: true, completion: nil)
+    }
+}
+
+extension CreateAccountVC: AuthUserServiceDelegate {
+    func didFailCreatingUser(_ authUserService: AuthUserService, error: String) {
+        presentErrorAlert(withErrorMessage: error)
+    }
+    func didFailEmailVerification(_ authUserService: AuthUserService, user: User, error: String) {
+        presentErrorAlert(withErrorMessage: error)
+    }
+    func didCreateUser(_ authUserService: AuthUserService, userProfile: UserProfile) {
+        let alert = Alert.create(withTitle: "Success!", andMessage: "Account Created\nA verification email has been sent to your email, \(self.createAccountView.emailTextField.text!) ", withPreferredStyle: .alert)
+        Alert.addAction(withTitle: "Back to Login", style: .default, andHandler: { (_) in
+            self.dismiss(animated: true, completion: nil)
+        }, to: alert)
+        present(alert, animated: true, completion: nil)
     }
 }
