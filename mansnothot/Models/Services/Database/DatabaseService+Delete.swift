ce0a7ffa70c4647f707420921fb3728b125ffd79
Melissa He <melissahe@ac.c4q.nyc>
2018-02-06 12:25:22 -0500

    Add the start of delete functions, start of updating functions

---
+++ mansnothot/Models/Services/Database/DatabaseService+Delete.swift
@@ -0,0 +1,15 @@
+//
+//  DatabaseService+Delete.swift
+//  mansnothot
+//
+//  Created by C4Q on 2/6/18.
+//  Copyright © 2018 Melissa He @ C4Q. All rights reserved.
+//
+
+import Foundation
+
+extension DatabaseService {
+    //for deleting posts
+    //for deleting comments
+    //for deleting users?? - if flagged too much??
+}
