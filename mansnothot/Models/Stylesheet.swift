254fc50ebc23b361347f96bc99cc45c1932b428c
Margaret Chan <margaret.helen.chan@gmail.com>
2018-02-06 23:37:04 -0500

    style sheet update, login view ui

--- mansnothot/Models/Stylesheet.swift
+++ mansnothot/Models/Stylesheet.swift
@@ -9,42 +9,151 @@
 import Foundation
 import UIKit
 
-class Stylesheet {
+enum Stylesheet {
     
     enum Colors {
         static let White = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha: 1.00)
         static let LightGrey = UIColor(red: 0.961, green: 0.961, blue: 0.961, alpha: 1.00)
-        static let Red = UIColor(red: 0.961, green: 0.961, blue: 0.961, alpha: 1.00)
+        static let Red = UIColor(red: 0.949, green: 0.141, blue: 0, alpha: 1.0)
         static let Orange = UIColor(red: 0.925, green: 0.471, blue: 0.235, alpha: 1.00)
-        static let Yellow = UIColor(red: 0.929, green: 0.714, blue: 0.384, alpha: 1.00)
+        static let Yellow = UIColor(red: 0.9569, green: 0.8627, blue: 0, alpha: 1.0)
         static let Dark = UIColor(red: 0.184, green: 0.157, blue: 0.118, alpha: 1.00)
     }
     
     enum Fonts {
-        static let Title = UIFont(name: "Optima", size: 50.0)
-        static let Regular = UIFont(name: "Helvetica Neue", size: 10.0)
-        static let Bold = UIFont(name: "Myriad", size: 25.0)
+        static let AppName = UIFont(name: "Optima", size: 50.0)
+        static let PostTitle = UIFont(name: "Optima", size: 20.0)
+        static let Regular = UIFont(name: "Helvetica Neue", size: 15.0)
+        static let Login = UIFont(name: "Helvetica Neue", size: 15.0)
+        static let Link = UIFont(name: "Helvetica Neue", size: 15.0)
+        static let Bold = UIFont(name: "Helvetica Bold", size: 15.0)
+        
     }
+}
+
+extension Stylesheet {
     
-    enum Contents {
-        enum Button {
-            static let FillColor = Stylesheet.Colors.Red
-            static let TextColor =  Stylesheet.Colors.Yellow
-            static let TextFont = Stylesheet.Fonts.Regular
+    enum Objects {
+        
+        
+        enum Buttons {
+            case Login
+            case ChangeLink
+            case CreateLink
+            
+            func style(button: UIButton) {
+                switch self {
+                case .Login:
+                    button.setTitle("Log In", for: .normal)
+                    button.setTitleColor(Stylesheet.Colors.LightGrey, for: .normal)
+                    button.showsTouchWhenHighlighted = true
+                    button.titleLabel?.font = Stylesheet.Fonts.Bold
+                    button.backgroundColor = Stylesheet.Colors.Red
+//                    button.layer.shadowOffset = CGSize(width: 1, height: 1)
+//                    button.layer.shadowRadius = 0.25
+//                    button.layer.shadowColor = (Stylesheet.Colors.Dark).cgColor
+//                    button.layer.shadowOpacity = 0.5
+                    button.layer.borderColor = (Stylesheet.Colors.Dark).cgColor
+                    button.layer.borderWidth = 1
+//                    button.layer.masksToBounds = true
+                case .ChangeLink:
+                    button.setTitleColor(Stylesheet.Colors.Orange, for: .normal)
+                    button.showsTouchWhenHighlighted = true
+                    button.titleLabel?.font = Stylesheet.Fonts.Link
+//                    button.layer.shadowOffset = CGSize(width: 1, height: 1)
+//                    button.layer.shadowRadius = 0.25
+//                    button.layer.shadowColor = (Stylesheet.Colors.Dark).cgColor
+//                    button.layer.shadowOpacity = 0.5
+                case .CreateLink:
+                    button.setTitleColor(Stylesheet.Colors.Red, for: .normal)
+                    button.showsTouchWhenHighlighted = true
+                    button.titleLabel?.font = Stylesheet.Fonts.Bold
+                    button.backgroundColor = Stylesheet.Colors.Yellow
+                    button.layer.borderColor = (Stylesheet.Colors.Orange).cgColor
+                    button.layer.borderWidth = 1
+//                    button.layer.shadowOffset = CGSize(width: 1, height: 1)
+//                    button.layer.shadowRadius = 0.25
+//                    button.layer.shadowColor = (Stylesheet.Colors.Dark).cgColor
+//                    button.layer.shadowOpacity = 0.5
+                }
+            }
         }
         
-        enum Post {
-            static let PostTitle = Stylesheet.Fonts.Title
-            static let CategoryTitle = Stylesheet.Fonts.Bold
-            static let UserNameTitle = Stylesheet.Fonts.Regular
+        enum Labels {
+            case Username
+            case AppName
+            case Category
+            case PostTitle
+            
+            func style(label: UILabel) {
+                switch self {
+                case .Username:
+                    label.textColor = Stylesheet.Colors.Dark
+                    label.font = Stylesheet.Fonts.Regular
+                case .AppName:
+                    label.font = Stylesheet.Fonts.AppName
+                    label.textColor = Stylesheet.Colors.Dark
+                case .Category:
+                    label.font = Stylesheet.Fonts.Bold
+                    label.textColor = Stylesheet.Colors.Dark
+                case .PostTitle:
+                    label.font = Stylesheet.Fonts.PostTitle
+                    label.textColor = Stylesheet.Colors.Dark
+                }
+            }
         }
+        
         
         enum Textfields {
-            static let BorderColor = Stylesheet.Colors.LightGrey
-            static let TextColor = Stylesheet.Colors.Dark
-            static let BorderType = UITextBorderStyle.line
-            static let TextAlignment = NSTextAlignment.left
+            case LoginEmail
+            case LoginPassword
+            case PostTitle
             
+            func style(textfield: UITextField) {
+                switch self {
+                case .LoginEmail:
+                    textfield.borderStyle = UITextBorderStyle.bezel
+                    textfield.layer.borderColor = (Stylesheet.Colors.LightGrey).cgColor
+                    textfield.backgroundColor = Stylesheet.Colors.White
+                    textfield.textAlignment = NSTextAlignment.left
+                    textfield.font = Stylesheet.Fonts.Login
+                    textfield.textColor = Stylesheet.Colors.Dark
+                    textfield.adjustsFontSizeToFitWidth = true
+                    textfield.autocapitalizationType = .none
+                    textfield.autocorrectionType = .no
+                    textfield.keyboardType = .emailAddress
+                    textfield.returnKeyType = .next
+                    textfield.placeholder = "Email Address"
+                case .LoginPassword:
+                    textfield.borderStyle = UITextBorderStyle.bezel
+                    textfield.layer.borderColor = (Stylesheet.Colors.LightGrey).cgColor
+                    textfield.backgroundColor = Stylesheet.Colors.White
+                    textfield.textAlignment = NSTextAlignment.left
+                    textfield.font = Stylesheet.Fonts.Login
+                    textfield.textColor = Stylesheet.Colors.Dark
+                    textfield.adjustsFontSizeToFitWidth = true
+                    textfield.autocapitalizationType = .none
+                    textfield.autocorrectionType = .no
+                    textfield.keyboardType = .asciiCapable
+                    textfield.returnKeyType = .done
+                    textfield.placeholder = "Password"
+                    textfield.isSecureTextEntry = true
+                case .PostTitle:
+                    textfield.borderStyle = UITextBorderStyle.bezel
+                    textfield.layer.borderColor = (Stylesheet.Colors.LightGrey).cgColor
+                    textfield.backgroundColor = Stylesheet.Colors.White
+                    textfield.textAlignment = NSTextAlignment.left
+                    textfield.font = Stylesheet.Fonts.Regular
+                    textfield.textColor = Stylesheet.Colors.Dark
+                    textfield.adjustsFontSizeToFitWidth = true
+                    textfield.autocapitalizationType = .sentences
+                    textfield.autocorrectionType = .yes
+                    textfield.keyboardType = .asciiCapable
+                    textfield.returnKeyType = .next
+                    textfield.placeholder = "Post Title"
+                }
+            }
         }
     }
 }
+
