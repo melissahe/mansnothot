62b96dc492a2ad102b64aeaf05ceffcccc98a07a
Richard Crichlow <richardcrichlow@ac.c4q.nyc>
2018-02-08 17:52:38 -0500

    Sharing to Email added for HomeFeedvc and MyPostvc

--- mansnothot/View Controllers/MyPostsVC.swift
+++ mansnothot/View Controllers/MyPostsVC.swift
@@ -10,6 +10,8 @@ import UIKit
 import SnapKit
 import TableFlip
 import Kingfisher
+import Social
+import MessageUI
 
 //Purpose: shows all of the user's posts
 
@@ -30,7 +32,7 @@ import Kingfisher
         //maybe we'll just have edit/delete buttons??
     //maybe should pass in the userID in the initializer so it can get all of the posts from firebase??
 
-class MyPostsVC: UIViewController {
+class MyPostsVC: UIViewController, MFMailComposeViewControllerDelegate {
     let editMyPost = EditMyPostVC()
     let myPostView = MyPostsView()
     
@@ -74,6 +76,95 @@ class MyPostsVC: UIViewController {
         DatabaseService.manager.getPosts(fromUID: uid, completion: { (myPosts) in
             self.posts = myPosts
         })
+    }
+    
+    @objc func showShareActionSheet(_ sender: UIButton){
+        if let cell = sender.superview as? MyPostsTableViewCell {
+            let alert = UIAlertController(title: "Share", message: nil, preferredStyle: .actionSheet)
+            let goToFacebook = UIAlertAction(title: "Facebook", style: .destructive, handler: {(UIAlertAction) -> Void in
+                print("Add Share To Facebook Function here")
+                // Check if user has Facebook
+                if SLComposeViewController.isAvailable(forServiceType: SLServiceTypeFacebook) {
+                    let post = SLComposeViewController(forServiceType: SLServiceTypeFacebook)!
+                    
+                    //Add a title to the post
+                    let title = cell.postTitleLabel.text
+                    post.setInitialText(title)
+                    
+                    //If there is an image, add it to the post
+                    if let image = cell.postImageView.image {
+                        post.add(image)
+                    }
+                    
+                    self.present(post, animated: true, completion: nil)
+                } else {
+                    self.showAlert(service: "Facebook")
+                }
+            })
+            let goToTwitter = UIAlertAction(title: "Twitter", style: .destructive, handler: {(UIAlertAction) -> Void in
+                print("Add Share to Twitter Function here")
+                // Check if user has Twitter
+                if SLComposeViewController.isAvailable(forServiceType: SLServiceTypeTwitter) {
+                    let post = SLComposeViewController(forServiceType: SLServiceTypeTwitter)!
+                    
+                    //Add a title to the post
+                    let title = cell.postTitleLabel.text
+                    post.setInitialText(title)
+                    
+                    //If there is an image, add it to the post
+                    if let image = cell.postImageView.image {
+                        post.add(image)
+                    }
+                    
+                    self.present(post, animated: true, completion: nil)
+                } else {
+                    self.showAlert(service: "Twitter")
+                }
+            })
+            let goToEmail = UIAlertAction(title: "Email", style: .destructive, handler: {(UIAlertAction) -> Void in
+                print("Add Share to Email Function here")
+                let mailComposeViewController = self.configuredMailComposeViewController()
+                if MFMailComposeViewController.canSendMail() {
+                    self.present(mailComposeViewController, animated: true, completion: nil)
+                } else {
+                    //self.showAlert(service: "Email") //MailController pops up its own alert with no email service
+                }
+            })
+            
+            let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: {(UIAlertAction) -> Void in
+                print("User cancelled")
+            })
+            
+            alert.addAction(goToFacebook)
+            alert.addAction(goToTwitter)
+            alert.addAction(goToEmail)
+            alert.addAction(cancel)
+            present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    func showAlert(service: String) {
+        let alert = UIAlertController(title: "Error", message: "You are not connected to \(service)", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Dismiss", style: .cancel, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func configuredMailComposeViewController() -> MFMailComposeViewController {
+        let mailComposerVC = MFMailComposeViewController()
+        mailComposerVC.mailComposeDelegate = self // Extremely important to set the --mailComposeDelegate-- property, NOT the --delegate-- property
+        
+        mailComposerVC.setToRecipients(["example@gmail.com"])
+        mailComposerVC.setSubject("Sending you an in-app e-mail with Professional Thoughts!")
+        mailComposerVC.setMessageBody("Sending e-mail with Professional Thoughts is the perfect user experience!", isHTML: false)
+        
+        return mailComposerVC
+    }
+    
+    // MARK: MFMailComposeViewControllerDelegate
+    private func mailComposeController(controller: MFMailComposeViewController!, didFinishWithResult result: MFMailComposeResult, error: NSError!) {
+        controller.dismiss(animated: true, completion: nil)
+        
     }
     
 }
@@ -122,6 +213,8 @@ extension MyPostsVC: UITableViewDataSource {
         cell.thumbsDownButton.addTarget(self, action: #selector(thumbsDownButtonTouched(_:)), for: .touchUpInside)
         cell.editButton.addTarget(self, action: #selector(editPost(_:)), for: .touchUpInside)
         cell.trashButton.addTarget(self, action: #selector(trashThatPost(_:)), for: .touchUpInside)
+        cell.shareButton.addTarget(self, action: #selector(showShareActionSheet), for: .touchUpInside)
+        cell.showArrowButton.addTarget(self, action: #selector(showShareActionSheet), for: .touchUpInside)
         return cell
     }
     
