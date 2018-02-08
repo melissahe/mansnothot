fbf1e2be2500df7e96d86f8ec0a82aecea9c4b0e
Margaret Chan <margaret.helen.chan@gmail.com>
2018-02-08 12:18:22 -0500

    UI cleanup NewPostView, CategoryTVC

--- mansnothot/Views/Cells/CategoryTableViewCell.swift
+++ mansnothot/Views/Cells/CategoryTableViewCell.swift
@@ -15,11 +15,12 @@ class CategoryTableViewCell: UITableViewCell {
     lazy var categoryLabel: UILabel = {
         let lb = UILabel()
         lb.text = "Category Here"
-        lb.backgroundColor = .white
-        lb.textAlignment = .center
-        lb.textColor = .black
-        lb.numberOfLines = 0
-        lb.layer.borderWidth = 0.5
+        Stylesheet.Objects.Labels.PostCategory.style(label: lb)
+//        lb.backgroundColor = .white
+//        lb.textAlignment = .center
+//        lb.textColor = .black
+//        lb.numberOfLines = 0
+//        lb.layer.borderWidth = 0.5
         return lb
     }()
     
