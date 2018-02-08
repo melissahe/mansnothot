80d23b5768a4b3627ba3cd26c5dbd8f0325e105f
Margaret Chan <margaret.helen.chan@gmail.com>
2018-02-07 20:13:54 -0500

    update UI FeedTVC, MyPostsTVC, MyPostsView, assets

--- mansnothot/Views/Cells/MyPostsTableViewCell.swift
+++ mansnothot/Views/Cells/MyPostsTableViewCell.swift
@@ -38,11 +38,12 @@ class MyPostsTableViewCell: UITableViewCell {
     lazy var postTitleLabel: UILabel = {
         let lb = UILabel()
         lb.text = "Post Title"
-        lb.backgroundColor = .green
-        lb.textAlignment = .left
-        lb.textColor = .black
-        lb.numberOfLines = 0
-        lb.layer.borderWidth = 0.5
+        Stylesheet.Objects.Labels.PostTitle.style(label: lb)
+//        lb.backgroundColor = .green
+//        lb.textAlignment = .left
+//        lb.textColor = .black
+//        lb.numberOfLines = 0
+//        lb.layer.borderWidth = 0.5
         return lb
     }()
     
@@ -50,12 +51,14 @@ class MyPostsTableViewCell: UITableViewCell {
     lazy var editButton: UIButton = {
         let eb = UIButton()
         eb.setImage(UIImage(named: "edit"), for: .normal)
+        Stylesheet.Objects.Buttons.ClearButton.style(button: eb)
         return eb
     }()
     
     lazy var trashButton: UIButton = {
         let tb = UIButton()
         tb.setImage(UIImage(named: "trash"), for: .normal)
+        Stylesheet.Objects.Buttons.ClearButton.style(button: tb)
         return tb
     }()
     
@@ -63,20 +66,22 @@ class MyPostsTableViewCell: UITableViewCell {
     lazy var postImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.image = nil
-        imageView.backgroundColor = .green
-        imageView.contentMode = .scaleAspectFit
-        imageView.layer.borderWidth = 0.5
+        Stylesheet.Objects.ImageViews.Opaque.style(imageView: imageView)
+//        imageView.backgroundColor = .green
+//        imageView.contentMode = .scaleAspectFit
+//        imageView.layer.borderWidth = 0.5
         return imageView
     }()
     
     //textView - for post
     lazy var postTextView: UITextView = {
         let tv = UITextView()
-        tv.layer.borderWidth = 0.5
         tv.text = "Sample Post Text Here"
-        tv.backgroundColor = .yellow
-        tv.textAlignment = .justified
-        tv.isEditable = false
+        Stylesheet.Objects.Textviews.Completed.style(textview: tv)
+//        tv.layer.borderWidth = 0.5
+//        tv.backgroundColor = .yellow
+//        tv.textAlignment = .justified
+//        tv.isEditable = false
         return tv
     }()
     
@@ -84,8 +89,9 @@ class MyPostsTableViewCell: UITableViewCell {
     lazy var thumbsUpButton: UIButton = {
         let button = UIButton()
         button.setImage(UIImage(named: "thumbsUp"), for: .normal)
-        button.backgroundColor = .clear
-        button.layer.borderWidth = 0.5
+        Stylesheet.Objects.Buttons.ClearButton.style(button: button)
+//        button.backgroundColor = .clear
+//        button.layer.borderWidth = 0.5
         return button
     }()
     
@@ -93,9 +99,10 @@ class MyPostsTableViewCell: UITableViewCell {
     lazy var numberOfLikesLabel: UILabel = {
         let lb = UILabel()
         lb.text = "+37"
-        lb.backgroundColor = .gray
-        lb.textAlignment = .center
-        lb.backgroundColor = .white
+        Stylesheet.Objects.Labels.LikesDislikes.style(label: lb)
+//        lb.backgroundColor = .gray
+//        lb.textAlignment = .center
+//        lb.backgroundColor = .white
         return lb
     }()
     
@@ -103,8 +110,9 @@ class MyPostsTableViewCell: UITableViewCell {
     lazy var thumbsDownButton: UIButton = {
         let button = UIButton()
         button.setImage(UIImage(named: "thumbsDown"), for: .normal)
-        button.backgroundColor = .clear
-        button.layer.borderWidth = 0.5
+        Stylesheet.Objects.Buttons.ClearButton.style(button: button)
+//        button.backgroundColor = .clear
+//        button.layer.borderWidth = 0.5
         return button
     }()
     
@@ -112,29 +120,32 @@ class MyPostsTableViewCell: UITableViewCell {
     lazy var numberOfDislikesLabel: UILabel = {
         let lb = UILabel()
         lb.text = "-7"
-        lb.backgroundColor = .gray
-        lb.textAlignment = .center
-        lb.backgroundColor = .white
+        Stylesheet.Objects.Labels.LikesDislikes.style(label: lb)
+//        lb.backgroundColor = .gray
+//        lb.textAlignment = .center
+//        lb.backgroundColor = .white
         return lb
     }()
     
     
-    //comment button - should segue to AddCommentVC //actually i'm not sure if we're doing this anymore, refer to trello!
+    //comment icon button
     lazy var commentButton: UIButton = {
         let button = UIButton()
         button.setImage(UIImage(named: "comment"), for: .normal)
-        button.backgroundColor = .clear
-        button.layer.borderWidth = 0.5
+        Stylesheet.Objects.Buttons.ClearButton.style(button: button)
+//        button.backgroundColor = .clear
+//        button.layer.borderWidth = 0.5
         return button
     }()
     
     //showThread button - should display the total number of comments in button title - should segue to AllCommentsVC
     lazy var showThreadButton: UIButton = {
         let button = UIButton()
-        button.setTitleColor(.black, for: .normal)
         button.setTitle("Thread", for: .normal)
-        button.backgroundColor = .orange
-        button.layer.borderWidth = 0.5
+        Stylesheet.Objects.Buttons.Link.style(button: button)
+//        button.setTitleColor(.black, for: .normal)
+//        button.backgroundColor = .orange
+//        button.layer.borderWidth = 0.5
         
         return button
     }()
@@ -143,10 +154,18 @@ class MyPostsTableViewCell: UITableViewCell {
     //Share Button
     lazy var shareButton: UIButton = {
         let button = UIButton()
-        button.setTitleColor(.black, for: .normal)
         button.setTitle("Share", for: .normal)
-        button.backgroundColor = .yellow
-        button.layer.borderWidth = 0.5
+        Stylesheet.Objects.Buttons.Link.style(button: button)
+//        button.setTitleColor(.black, for: .normal)
+//        button.backgroundColor = .yellow
+//        button.layer.borderWidth = 0.5
+        return button
+    }()
+    
+    // share arrow button
+    lazy var showArrowButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "sharearrow"), for: .normal)
         return button
     }()
     
@@ -165,9 +184,8 @@ class MyPostsTableViewCell: UITableViewCell {
     
     private func commonInit() {
         backgroundColor = .white
-        contentView.layer.borderColor = UIColor(red: 0.141, green: 0.149, blue: 0.184, alpha: 1.00).cgColor
-        contentView.layer.backgroundColor = UIColor(red: 0.2343, green: 0.56, blue: 0.344, alpha: 1.00).cgColor
-        contentView.layer.borderWidth = 2
+        contentView.layer.borderColor = Stylesheet.Colors.LightGrey.cgColor
+        contentView.layer.borderWidth = 1
         setupAndConstrainObjects()
     }
     
@@ -185,76 +203,88 @@ class MyPostsTableViewCell: UITableViewCell {
         self.addSubview(showThreadButton)
         self.addSubview(commentButton)
         self.addSubview(shareButton)
+        self.addSubview(showArrowButton)
         
        
         categoryLabel.snp.makeConstraints { (make) -> Void in
-            make.top.equalTo(self.snp.top).offset(5)
-            make.leading.equalTo(safeAreaLayoutGuide).offset(16)
+            make.top.equalTo(self.snp.top).offset(15)
+            make.leading.equalTo(safeAreaLayoutGuide.snp.leading).offset(5)
         }
         
         postTitleLabel.snp.makeConstraints { (make) -> Void in
-            make.leading.equalTo(safeAreaLayoutGuide).offset(5)
-            make.top.equalTo(categoryLabel.snp.bottom).offset(10)
+            make.leading.equalTo(self.snp.leading).offset(5)
+            make.top.equalTo(trashButton.snp.bottom).offset(5)
             make.trailing.equalTo(self.snp.trailing).offset(-5)
         }
         editButton.snp.makeConstraints { (make) -> Void in
-            make.top.equalTo(safeAreaLayoutGuide.snp.top).offset(2)
+//            make.top.equalTo(self.snp.top).offset(5)
+            make.centerY.equalTo(categoryLabel.snp.centerY)
             make.trailing.equalTo(trashButton.snp.leading).offset(-5)
-            make.height.equalTo(safeAreaLayoutGuide.snp.height).multipliedBy(0.10)
+//            make.height.equalTo(safeAreaLayoutGuide.snp.height).multipliedBy(0.10)
 //           make.width
         }
         trashButton.snp.makeConstraints { (make) -> Void in
-            make.top.equalTo(safeAreaLayoutGuide.snp.top).offset(2)
-            make.trailing.equalTo(safeAreaLayoutGuide).offset(-5)
-            make.height.equalTo(safeAreaLayoutGuide.snp.height).multipliedBy(0.10)
+//            make.top.equalTo(self.snp.top).offset(5)
+            make.centerY.equalTo(categoryLabel.snp.centerY)
+            make.trailing.equalTo(self.snp.trailing).offset(-5)
+//            make.height.equalTo(safeAreaLayoutGuide.snp.height).multipliedBy(0.10)
         }
         postImageView.snp.makeConstraints { (make) -> Void in
             make.top.equalTo(postTitleLabel.snp.bottom).offset(5)
             make.centerX.equalTo(self.snp.centerX)
+//            make.height.lessThanOrEqualTo(200)
             make.width.equalTo(self.snp.width).multipliedBy(0.5)
             make.height.equalTo(self.snp.height).multipliedBy(0.3)
             //make.height.equalTo(self.snp.height).multipliedBy(0.16)
         }
         postTextView.snp.makeConstraints { (make) -> Void in
-            make.width.equalTo(self.snp.width).multipliedBy(0.8)
-            make.height.equalTo(self.snp.height).multipliedBy(0.3)
-            make.centerX.equalTo(self.snp.centerX)
             make.top.equalTo(postImageView.snp.bottom).offset(5)
+            make.leading.equalTo(self.snp.leading).offset(5)
+            make.trailing.equalTo(self.snp.trailing).offset(-5)
+            make.height.equalTo(self.snp.height).multipliedBy(0.3)
+//            make.width.equalTo(self.snp.width).multipliedBy(0.8)
+//            make.height.equalTo(self.snp.height).multipliedBy(0.3)
+//            make.centerX.equalTo(self.snp.centerX)
         }
         thumbsUpButton.snp.makeConstraints { (make) -> Void in
-            make.bottom.equalTo(self.snp.bottom).offset(-5)
+            make.centerY.equalTo(showThreadButton.snp.centerY)
             make.leading.equalTo(self.snp.leading).offset(5)
         }
         numberOfLikesLabel.snp.makeConstraints { (make) -> Void in
-            make.leading.equalTo(thumbsUpButton.snp.trailing)
+            make.leading.equalTo(thumbsUpButton.snp.trailing).offset(2)
             make.bottom.equalTo(self.snp.bottom).offset(-5)
-            make.height.equalTo(postTitleLabel.snp.height)
+            make.height.equalTo(self.snp.width).multipliedBy(0.07)
         }
         thumbsDownButton.snp.makeConstraints { (make) -> Void in
-            make.leading.equalTo(numberOfLikesLabel.snp.trailing)
-            make.bottom.equalTo(self.snp.bottom).offset(-5)
+            make.leading.equalTo(numberOfLikesLabel.snp.trailing).offset(2)
+            make.centerY.equalTo(showThreadButton.snp.centerY)
         }
         numberOfDislikesLabel.snp.makeConstraints { (make) -> Void in
-            make.leading.equalTo(thumbsDownButton.snp.trailing)
+            make.leading.equalTo(thumbsDownButton.snp.trailing).offset(2)
             make.bottom.equalTo(self.snp.bottom).offset(-5)
-            make.height.equalTo(postTitleLabel.snp.height)
+            make.height.equalTo(self.snp.width).multipliedBy(0.07)
         }
         showThreadButton.snp.makeConstraints { (make) -> Void in
-            make.bottom.equalTo(self.snp.bottom).offset(-5)
-            make.height.equalTo(numberOfDislikesLabel.snp.height)
-            make.width.equalTo(self.snp.width).multipliedBy(0.18)
-            make.centerX.equalTo(self.snp.centerX)
+            make.centerY.equalTo(commentButton.snp.centerY)
+            make.leading.equalTo(commentButton.snp.trailing).offset(2)
         }
         commentButton.snp.makeConstraints { (make) -> Void in
             make.bottom.equalTo(self.snp.bottom).offset(-5)
-            make.leading.equalTo(showThreadButton.snp.trailing)
+            make.centerX.equalTo(self.snp.centerX)
         }
-        shareButton.snp.makeConstraints { (make) -> Void in
+        
+        showArrowButton.snp.makeConstraints { (make) -> Void in
             make.top.equalTo(postTextView.snp.bottom).offset(5)
-            make.trailing.equalTo(self.snp.trailing).offset(-5)
+            make.trailing.equalTo(shareButton.snp.leading).offset(-2)
             make.bottom.equalTo(self.snp.bottom).offset(-5)
-            make.height.equalTo(numberOfLikesLabel.snp.height)
-            make.width.equalTo(self.snp.width).multipliedBy(0.18)
+            
+        }
+        
+        shareButton.snp.makeConstraints { (make) -> Void in
+//            make.top.equalTo(postTextView.snp.bottom).offset(5)
+            make.trailing.equalTo(self.snp.trailing).offset(-5)
+            make.centerY.equalTo(commentButton.snp.centerY)
+//            make.bottom.equalTo(self.snp.bottom).offset(-5)
         }
     }
 }
